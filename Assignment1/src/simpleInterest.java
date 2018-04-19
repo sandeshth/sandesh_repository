098eb9d7031b67f3adbe01de9d1a5ece94d677a1
Sandesh Thapa <Sandesh Thapa@DESKTOP-R5VQC47>
2018-04-19 12:51:57 -0500

    Edited version with the use of method.

--- Assignment1/src/simpleInterest.java
+++ Assignment1/src/simpleInterest.java
@@ -1,37 +1,39 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 /*
  * This programme calculate the simple interest as per the user input.
  */
 
-public class simpleInterest 
-{
-	public static void main(String[] args)
-	{
-	int principle, time;
-	double rate;
-	double simpleinterest;
-	
-	Scanner keyboard = new Scanner(System.in);
-	
-	// Get the user's principle
-	System.out.print("Enter the principle: ");
-	principle = keyboard.nextInt();
-	
-	// Get the user's time
+public class simpleInterest {
+
+	public static void main(String[] args) {
+		SimpleInterest();
+	}
+
+	public static void SimpleInterest() {
+		double principle;
+		int time;
+		double rate;
+		double simpleinterest;
+
+		Scanner keyboard = new Scanner(System.in);
+
+		// Get the user's principle
+		System.out.print("Enter the principle: ");
+		principle = keyboard.nextInt();
+
+		// Get the user's time
 		System.out.print("Enter the time in years: ");
 		time = keyboard.nextInt();
-		
+
 		// Get the user's rate
 		System.out.print("Enter the rate: ");
 		rate = keyboard.nextDouble();
-		
+
 		rate = rate / 100;
-		
+
 		simpleinterest = (principle * time * rate) / 100;
-		
+
 		System.out.println("The simple interest is " + simpleinterest);
 	}
-		
-	
 }
